526b96ce4488c2efa4c4b1f6cf32f254feb8adf9
Paraidomat <S-k-y.F-i-r-e@gmx.de>
2018-08-16 10:30:38 +0200

    Update create_files.sh

--- create_files.sh
+++ create_files.sh
@@ -1,1 +1,1 @@
-pandoc --toc --template="./template.html" -s -o todo_liste.html 000yaml.yaml $(ls | grep .md | grep -v README | sort)
+pandoc --toc --template="./template.html" -s -o todo_list.html 000yaml.yaml $(ls | grep .md | grep -v README | sort)
