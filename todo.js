aad6caf05c56a2469a69a1e4992c427f0ba130fe
Paraidomat <S-k-y.F-i-r-e@gmx.de>
2018-09-05 17:27:50 +0200

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,25 +1,29 @@
 $(document).ready(function(){
-    //TOC Stuff
+    // TOC Stuff
     $("a").click(function(){
-        console.log("Slide everything up");
         $("h2").nextUntil("h1, h2").slideUp();
 
-        console.log("Get clicked Target and slideToggle it!");
         var target = $( this ).attr('href');
         $(target).nextUntil("h1, h2").slideToggle();
-
-        console.log("Everything is done!");
     });
 
     $("nav").click(function(){
-        console.log(this);
         $( this ).find("ul").slideToggle();
     });
 
-    //Heading Stuff
+    // Heading Stuff
     $("h2").nextUntil("h1, h2").slideUp();
-    //$("h2").nextUntil("h2").slideUp();
     $("h2").click(function(){
         $( this ).nextUntil("h1, h2").slideToggle();
+    });
+
+    // Buttons
+    $("#expandall").click(function(){
+        $("h2").nextUntil("h1, h2").slideDown();
+        $("nav").find("ul").slideDown();
+    });
+    $("#collapseall").click(function(){
+        $("h2").nextUntil("h1, h2").slideUp();
+        $("nav").find("ul").slideUp();
     });
 });
